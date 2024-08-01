2400e6ea4db512e6e642b6afeae613f6ce17e438
erendiro pedro <erendiropedro@gmail.com>
2024-08-01 10:19:52 +0200

    progress bar, pos-processing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,54 @@
 #include "headers/TetrahedronFactory.h"
 #include "headers/Types.h"
 
+void post_processing(std::unique_ptr<BaseWriter>& writer, const std::string& format) {
+    if (format == "obj") {
+        OBJWriter* obj_writer = dynamic_cast<OBJWriter*>(writer.get());
+        if (obj_writer != nullptr) {
+            obj_writer->zipDirectory();
+        }
+    } if (format == "csv") {
+    //     std::string filename = "your_file.csv"; // Replace with your filename
+    //     std::ifstream inFile(filename);
+    //     std::vector<std::tuple<double, std::string>> data;
+
+    //     std::string line;
+    //     while (std::getline(inFile, line)) {
+    //         std::istringstream iss(line);
+    //         std::string tetrahedron1, tetrahedron2, intersectionStatus;
+    //         if (!(iss >> tetrahedron1 >> tetrahedron2 >> intersectionStatus)) { break; } // error
+
+    //         // Parse the x coordinate of the first vertex of the first tetrahedron
+    //         // This assumes that the tetrahedron is represented as a string "x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4"
+    //         std::string::size_type sz;
+    //         double x = std::stod(tetrahedron1, &sz);
+    //         data.push_back(std::make_tuple(x, line));
+    //     }
+
+    //     // Sort the data
+    //     std::sort(data.begin(), data.end());
+
+    //     // Write the sorted data back out to the CSV file
+    //     std::ofstream outFile(filename);
+    //     for (const auto& row : data) {
+    //         outFile << std::get<1>(row) << "\n";
+    // }
+    }
+}
+
+void print_progress_bar(int i, int NUMBER_OF_ENTRIES) {
+    // Print progress bar
+    std::cout << "\rProgress: [";
+    int pos = static_cast<int>(static_cast<float>(i) / NUMBER_OF_ENTRIES * 50);
+    for (int j = 0; j < 50; ++j) {
+        if (j < pos) std::cout << "=";
+        else if (j == pos) std::cout << ">";
+        else std::cout << " ";
+    }
+    std::cout << "] " << int(static_cast<float>(i) / NUMBER_OF_ENTRIES * 100.0) << " %";
+    std::cout.flush();
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <NUMBER_OF_ENTRIES> <FORMAT>" << std::endl;
@@ -51,53 +99,12 @@ int main(int argc, char* argv[]) {
         double intersection_volume = GeometryUtils::getIntersectionVolume(*tetrahedron1, *tetrahedron2);
         writer->writeEntry(*tetrahedron1, *tetrahedron2, intersection_volume, intersection_status);
 
-        // Print progress bar
-        std::cout << "\rProgress: [";
-        int pos = static_cast<int>(static_cast<float>(i) / NUMBER_OF_ENTRIES * 50);
-        for (int j = 0; j < 50; ++j) {
-            if (j < pos) std::cout << "=";
-            else if (j == pos) std::cout << ">";
-            else std::cout << " ";
-        }
-        std::cout << "] " << int(static_cast<float>(i) / NUMBER_OF_ENTRIES * 100.0) << " %";
-        std::cout.flush();
-
-        }
-
-    if (format == "obj") {
-        OBJWriter* obj_writer = dynamic_cast<OBJWriter*>(writer.get());
-        if (obj_writer != nullptr) {
-            obj_writer->zipDirectory();
-        }
-    } if (format == "csv") {
-    //     std::string filename = "your_file.csv"; // Replace with your filename
-    //     std::ifstream inFile(filename);
-    //     std::vector<std::tuple<double, std::string>> data;
-
-    //     std::string line;
-    //     while (std::getline(inFile, line)) {
-    //         std::istringstream iss(line);
-    //         std::string tetrahedron1, tetrahedron2, intersectionStatus;
-    //         if (!(iss >> tetrahedron1 >> tetrahedron2 >> intersectionStatus)) { break; } // error
-
-    //         // Parse the x coordinate of the first vertex of the first tetrahedron
-    //         // This assumes that the tetrahedron is represented as a string "x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4"
-    //         std::string::size_type sz;
-    //         double x = std::stod(tetrahedron1, &sz);
-    //         data.push_back(std::make_tuple(x, line));
-    //     }
-
-    //     // Sort the data
-    //     std::sort(data.begin(), data.end());
-
-    //     // Write the sorted data back out to the CSV file
-    //     std::ofstream outFile(filename);
-    //     for (const auto& row : data) {
-    //         outFile << std::get<1>(row) << "\n";
-    // }
+        print_progress_bar(i + 1, NUMBER_OF_ENTRIES);
     }
 
+    post_processing(writer, format);
+
     std::cout << std::endl;
-    
+
     return 0;
 }
